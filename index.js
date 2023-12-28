a0c651b1e09be3d58e60ff8669c2cf0ee4801850
Valera Lykhatskiy <potniyschenok@gmail.com>
2023-12-28 22:19:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
       try {
         const newContact = { name, email, phone };
         await contactServices.addContact(newContact);
-        console.table([newContact]);
+        console.table([contactServices.addContact]);
       } catch (error) {
         console.error("error:", error.message);
       }
